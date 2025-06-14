4796323ed15c395d4a0378328b037fc3d9e6c9b5
Nik Revenco <pm@nikrev.com>
2025-06-14 23:35:41 +0100

    feat: add WIP scheme plugin part

---
+++ cog.scm
@@ -0,0 +1,29 @@
+(require "helix/components.scm")
+(require "helix/editor.scm")
+(require-builtin helix/core/text as text.)
+(require-builtin helix/core/static as static.)
+
+; Get a `Selection` of the current file
+(define (get-current-selection) (static.current-selection-object))
+
+; Get a `String` representing the text of the current file
+(define (get-current-text)
+  ; `rope->string` takes `Rope`
+  ;   return `String`
+  (text.rope->string
+    ; `editor->text` takes DocumentId
+    ;   return `Rope`
+    (editor->text
+      ; `editor->doc-id` takes `ViewId`
+      ;   return `DocumentId`
+      (editor->doc-id
+        ; `editor-focus`
+        ;   return `ViewId` of focused
+        (editor-focus)))))
+
+; This is where we want to send things to the GhostText server
+(define (handle-post-command name-of-command)
+  (log::info! (get-current-text)))
+
+; When we run any command in Helix, send it to the Ghost Text server
+(register-hook! "post-command" handle-post-command)
