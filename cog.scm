424bb487f95565b58dfbea6890176c8e3e61e122
Nik Revenco <pm@nikrev.com>
2025-06-17 13:40:16 +0100

    remove this commit

--- cog.scm
+++ cog.scm
@@ -3,3 +3,23 @@
 
 (define dependencies '())
 (define dylibs '((#:name "ghost-text")))
+
+(define dylibs
+  '((#:name "ghost-text"
+     #:urls
+     (
+      (
+       #:platform
+       "windows"
+       #:url
+       "https://github.com/nik-rev/ghost-text.hx/releases/download/v0.1.2/ghost_text.dll")
+      (
+       #:platform
+       "macos"
+       #:url
+       "https://github.com/nik-rev/ghost-text.hx/releases/download/v0.1.2/libghost_text.dylib")
+      (
+       #:platform
+       "linux"
+       #:url
+       "https://github.com/nik-rev/ghost-text.hx/releases/download/v0.1.2/libghost_text.so")))))
